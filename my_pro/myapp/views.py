8a391fee73c7e7e80c03709eee2c1506c2d77159
Bahisht-Ayaz <bahishtayaza9@gmail.com>
2025-10-14 13:03:15 +0500

    commit

---
+++ my_pro/myapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+url="https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]"
+url="https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]"
